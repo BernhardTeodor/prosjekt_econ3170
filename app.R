193d255d08ad0b23377951068068669ffa484470
Bernhard Teodor Thodesen <bernhatt@ulrik.uio.no>
2024-11-26 13:41:06 +0100

    opdatering app

--- app.R
+++ app.R
@@ -1,14 +1,15 @@
 library(shiny)
 
-source("koding.r") # Get lasso_fit
+source("app_data.r") # henter modell
 
 ui <- pageWithSidebar(
-  headerPanel("Tittel??"),
+  headerPanel("Titanic overlevelseskalkulator"),
   sidebarPanel(
                 numericInput("Age", label = "Alder", value = 10),
                 selectInput("Pclass", label = "Klasse", c("1", "2", "3")),
                 selectInput("Sex", label = "Kjønn", c("male", "female")),
-                sliderInput("Fare", label = "Bilettpris", min = 1, max = 500, value = 15),
+                selectInput("Fare", label = "Bilettpris", c("Lav", "Middels", "Høy")),
+                #sliderInput("Fare", label = "Bilettpris", min = 1, max = 500, value = 15),
                 selectInput("Embarked", label = "Gikk om bord", 
                             c("S", "C", "Q")),
                 numericInput("family_size", label = "Familie", value = 1),
@@ -27,13 +28,29 @@ server <- function(input, output, session) {
   datasetInput <- eventReactive(input$submit, {
   
     ny_tibble <- tibble(
+      
       Pclass = as.factor(input$Pclass),
-      Sex = input$Sex,
+      
+      Sex = as.factor(input$Sex),
+      
       Age = input$Age,
-      Fare = input$Fare,
+      
+      #Fare = input$Fare,
+      Fare = case_when(
+        input$Fare == "Høy" ~ 31,
+        input$Fare == "Middels" ~ 14.5,
+        input$Fare == "Lav" ~ 7.5),
+      
       Embarked = as.factor(input$Embarked),
-      family_size = input$family_size,
-      alone = as.factor(ifelse(input$family_size == 1, 1, 0)))
+      
+      Person_per_ticket = input$family_size,
+      
+      Minor = as.factor(ifelse(input$Age < 18, 1, 0)),
+      
+      Family_size = input$family_size,
+      
+      Alone = as.factor(ifelse(input$family_size == 1, 1, 0))
+      )
     
     output <- predict(lasso_fit, ny_tibble, type = "prob")
   })
---
+++ app_data.R
@@ -0,0 +1,125 @@
+library(tidyverse)
+
+titanic <- read_csv("Titanic-Dataset.csv")
+titanic <- titanic |> 
+  select(-Cabin)
+
+
+#Lager lister til å fylle inn verdiene
+master.mean <- list() 
+mr.mean <- list()
+mrs.mean <- list()
+miss.mean <- list()
+
+
+#Setter opp en for-loop til å finne gjennomsnittet for alle med en gitt tittel, i en gitt klasse
+for (i in 1:3){
+  for (j in c("Master", "Mr.", "Mrs.", "Miss")) {
+    if (j == "Master"){
+      master.mean[[i]] <- titanic |> 
+        filter(grepl("Master.", Name, fixed = TRUE), Pclass == i, Sex == "male") |> 
+        summarise(mean_age = mean(Age, na.rm = TRUE)) |> 
+        pull(mean_age)
+    } else if(j == "Mr."){
+      mr.mean[[i]] <- titanic |> 
+        filter(grepl("Mr.", Name, fixed = TRUE), Pclass == i, Sex == "male") |> 
+        summarise(mean_age = mean(Age, na.rm = TRUE)) |> 
+        pull(mean_age)
+    } else if (j == "Mrs.") {
+      mrs.mean[[i]] <- titanic |> 
+        filter(grepl("Mrs.", Name, fixed = TRUE), Pclass == i, Sex == "female") |> 
+        summarise(mean_age = mean(Age, na.rm = TRUE)) |> 
+        pull(mean_age)
+    } else if(j == "Miss") {
+      miss.mean[[i]] <- titanic |> 
+        filter(grepl("Miss.", Name, fixed = TRUE), Pclass == i, Sex == "female") |> 
+        summarise(mean_age = mean(Age, na.rm = TRUE)) |> 
+        pull(mean_age)
+    }
+  }
+}
+
+
+#Fyller inn de manglende verdiene
+for (i in 1:3){
+  titanic <- titanic |> 
+    mutate(Age = ifelse(grepl("Mr.", Name, fixed = T) & Pclass == i & is.na(Age), mr.mean[[i]], Age)) |> 
+    mutate(Age = ifelse(grepl("Miss", Name, fixed = T) & Pclass == i & is.na(Age), miss.mean[[i]], Age)) |> 
+    mutate(Age = ifelse(grepl("Mrs.", Name, fixed = T) & Pclass == i & is.na(Age), mrs.mean[[i]], Age)) |> 
+    mutate(Age = ifelse(grepl("Master", Name, fixed = T) & Pclass == i & is.na(Age), master.mean[[i]], Age))
+}
+
+
+mean.dr.p1 <- titanic |> 
+  filter(grepl("Dr.", Name, fixed = TRUE) & Pclass == 1) |> 
+  summarise(round(mean(Age, na.rm = TRUE))) |> 
+  pull()
+
+#Setter inn for verdien
+titanic <- titanic |> 
+  mutate(Age = ifelse(is.na(Age), mean.dr.p1, Age))
+
+
+tilfeldig_embarked <- sample(c("C", "S"), size = 1)
+
+titanic <- titanic |> 
+  mutate(Embarked = ifelse(is.na(Embarked),
+                           yes = tilfeldig_embarked,
+                           no = Embarked))
+
+titanic <- titanic |> 
+  add_count(Ticket, name = "Person_per_ticket") |> 
+  mutate(Minor = ifelse(Age < 18, 1, 0),
+         Family_size = SibSp+Parch,
+         Alone = ifelse(Family_size == 0, 1, 0)) |> 
+  select(-c(Name, Ticket, SibSp, Parch, PassengerId)) |> 
+  mutate(Pclass = as.factor(Pclass),
+         Alone = as.factor(Alone),
+         Survived = as.factor(Survived),
+         Minor = as.factor(Minor),
+         Sex = as.factor(Sex),
+         Embarked = as.factor(Embarked))
+
+set.seed(3170)
+titanic_split<- initial_split(titanic, prop = .8, strata = Survived)
+titanic_train <- training(titanic_split)
+titanic_test <- testing(titanic_split)
+titanic_split
+
+
+titanic_recipe <- 
+  recipe(Survived ~ ., data = titanic_train) |>
+  step_dummy(Sex, Embarked, Alone, Pclass, Minor)
+
+
+
+
+lasso_model <- 
+  logistic_reg(penalty = tune(), mixture = 1) |> 
+  set_engine("glmnet") |> 
+  set_mode("classification")
+
+
+wflow_lasso <- 
+  workflow() |> 
+  add_recipe(titanic_recipe) |> 
+  add_model(lasso_model)
+
+folds <- vfold_cv(titanic_train, 5, strata = Survived)
+
+penalty_grid <- grid_regular(penalty(range = c(-4, -1)), levels = 100)
+
+doParallel::registerDoParallel() # Parallelprogramering for raskre tune
+
+lasso_tune <- 
+  wflow_lasso |> 
+  tune_grid(resamples = folds, grid = penalty_grid)
+
+
+
+beste_lamda <- select_best(lasso_tune, metric = "roc_auc")
+beste_lamda
+
+lasso_fit <- finalize_workflow(wflow_lasso, beste_lamda) |> 
+  fit(data = titanic_train)
+
